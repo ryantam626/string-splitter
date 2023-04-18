bec7351ed385923a9e6b53a30ee61099c96088e3
Ryan Tam <ryantam626@gmail.com>
2023-04-18 07:34:44 +0100

    Don't run so many benchmarks

--- bench_lorem.py
+++ bench_lorem.py
@@ -1,11 +1,22 @@
 import os
 
-import pyximport; pyximport.install()
+import pyximport
 
-from implementation import python_re, python_native, python_np_mask, cython_generator, cython_class, cython_generator2, cython_generator3
+pyximport.install()
+
+from implementation import (
+    python_re,
+    python_native,
+    python_np_mask,
+    cython_generator,
+    cython_class,
+    cython_generator2,
+    cython_generator3,
+    cython_generator4,
+)
 
 
-with open('./fixtures/lorem30.txt', 'r') as fd:
+with open("./fixtures/lorem30.txt", "r") as fd:
     content = fd.read()
 
 
@@ -14,14 +25,14 @@ def base_runner():
         python_re.splitter(content)
 
 
-# def cython_class_runner():
-#     for _ in range(int(os.environ.get("REPEATS", 20))):
-#         cython_class.splitter(content)
-#
-#
-# def cython_generator_runner():
-#     for _ in range(int(os.environ.get("REPEATS", 20))):
-#         cython_generator.splitter(content)
+def cython_class_runner():
+    for _ in range(int(os.environ.get("REPEATS", 20))):
+        cython_class.splitter(content)
+
+
+def cython_generator_runner():
+    for _ in range(int(os.environ.get("REPEATS", 20))):
+        cython_generator.splitter(content)
 
 
 def cython_generator2_runner():
@@ -34,21 +45,27 @@ def cython_generator3_runner():
         cython_generator3.splitter(content)
 
 
-# def python_np_mask_runner():
-#     for _ in range(int(os.environ.get("REPEATS", 20))):
-#         python_np_mask.splitter(content)
-#
-#
-# def python_native_runner():
-#     for _ in range(int(os.environ.get("REPEATS", 20))):
-#         python_native.splitter(content)
+def cython_generator4_runner():
+    for _ in range(int(os.environ.get("REPEATS", 20))):
+        cython_generator4.splitter(content)
+
+
+def python_np_mask_runner():
+    for _ in range(int(os.environ.get("REPEATS", 20))):
+        python_np_mask.splitter(content)
+
+
+def python_native_runner():
+    for _ in range(int(os.environ.get("REPEATS", 20))):
+        python_native.splitter(content)
 
 
 __benchmarks__ = [
     # (base_runner, cython_class_runner, "Cython class"),
-    # (base_runner, cython_generator_runner, "Cython generator"),
+    (base_runner, cython_generator_runner, "Cython generator"),
     (base_runner, cython_generator2_runner, "Cython generator 2"),
     (base_runner, cython_generator3_runner, "Cython generator 3"),
+    (base_runner, cython_generator4_runner, "Cython generator 4"),
     # (base_runner, python_np_mask_runner, "Python Numpy mask"),
     # (base_runner, python_native_runner, "Python native"),
 ]
